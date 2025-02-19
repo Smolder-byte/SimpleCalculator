49b43480d72c4687221953b49dc3ab540298f2e1
Smolder-byte <osemen05@gmail.com>
2025-02-19 11:50:25 +0300

    Create ProjectB.java

---
+++ projectB..gitignore/src/projectb/ProjectB.java
@@ -0,0 +1,16 @@
+package projectb;
+
+/**
+ *
+ * @author syoma
+ */
+public class ProjectB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
