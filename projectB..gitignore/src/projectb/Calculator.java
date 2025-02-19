44540b963f250bb0820e23f6f6c90225093ea6ca
Smolder-byte <osemen05@gmail.com>
2025-02-19 13:25:13 +0300

    After_Merge

--- projectB..gitignore/src/projectb/Calculator.java
+++ projectB..gitignore/src/projectb/Calculator.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projectb;
 
-/**
- *
- * @author syoma
- */
 public class Calculator {
 
  public  double multiply(double a, double b)   {
     return a*b;
-
+}
 
     public double power (double a, double b) {
         return Math.pow(a,b);
@@ -27,12 +19,9 @@ public class Calculator {
         return a - b;
     }
     
-
-
-}
    public  double divide(double a, double b)   {
     return a/b;
     
 }  
- }
+}
   
