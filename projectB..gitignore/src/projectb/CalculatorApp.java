16e025912c1897d751b40990c1e32de548cf8182
Smolder-byte <osemen05@gmail.com>
2025-02-19 13:26:49 +0300

    Delete errors

--- projectB..gitignore/src/projectb/CalculatorApp.java
+++ projectB..gitignore/src/projectb/CalculatorApp.java
@@ -9,12 +9,7 @@ package projectb;
  * @author syoma
  */
 public class CalculatorApp {
-
-    
-}
-
-
-        public static void main(String[] args) {
+     public static void main(String[] args) {
             
         }
 
